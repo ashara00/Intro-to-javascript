0d000184b07d46b5698a29f6c07f2095c91d20b4
ashara00 <100097144+ashara00@users.noreply.github.com>
2022-06-23 22:56:43 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+alert("My name is Ashara Chidubem Samuel")
+alert("My height currently is 6.5ft")
+alert("I hail from Nigeria")
+
+console.log("My name is Ashara Chidubem Samuel")
+console.log("My height currently is 6.5ft")
+console.log("I hail from Nigeria")
+
+
+
